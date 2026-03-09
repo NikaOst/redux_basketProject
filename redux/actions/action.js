343459a3a06700d35831d6ea26b4f31c77257f94
VOstroverkh <veronikaostroverkh@gmail.com>
2026-03-09 13:18:41 +0100

    ADD: reducer, action, store

--- redux/actions/action.js
+++ redux/actions/action.js
@@ -1,14 +1,23 @@
-// export const addTodoAction = (data) => {
-//   return {
-//     type: 'ADD_TODO',
-//     payload: data,
-//   };
-// };
-// export const deleteTodoAction = (id) => {
-//   return {
-//     type: 'DELETE_TODO',
-//     payload: {
-//       id,
-//     },
-//   };
-// };
+export const addToCart = (data) => {
+  return {
+    type: 'ADD_TO_CART',
+    payload: data,
+  };
+};
+export const removeFromCart = (id) => {
+  return {
+    type: 'DELETE_FROM_CART',
+    payload: {
+      id,
+    },
+  };
+};
+export const updateCartQuantity = (name, quantity) => {
+  return {
+    type: 'UPDATE_QUANTITY',
+    payload: {
+      name,
+      quantity,
+    },
+  };
+};
--- redux/reducers/reducer.js
+++ redux/reducers/reducer.js
@@ -1,20 +1,31 @@
-// const initialState = {
-//   data: [],
-// };
-// const todoReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case 'ADD_TODO':
-//       return {
-//         ...state,
-//         data: [...state.data, action.payload],
-//       };
-//     case 'DELETE_TODO':
-//       return {
-//         ...state,
-//         data: state.data.filter((todo) => todo.id !== action.payload.id),
-//       };
-//     default:
-//       return state;
-//   }
-// };
-// export default todoReducer;
+const initialState = {
+  products: [],
+  basket: [],
+};
+const basketReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return {
+        ...state,
+        basket: [...state.basket, action.payload],
+      };
+    case 'REMOVE_FROM_CART':
+      return {
+        ...state,
+        basket: state.basket.filter((product) => product.id !== action.payload.id),
+      };
+    case 'UPDATE_CART_QUANTITY':
+      return {
+        ...state,
+        basket: state.basket.map((product) => {
+          if (product.name === action.payload.name) {
+            product.quantity = action.payload.quantity;
+          }
+          return product;
+        }),
+      };
+    default:
+      return state;
+  }
+};
+export default basketReducer;
